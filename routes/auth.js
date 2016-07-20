7b0654e76738f31b0f61ec50fd4c536102ca667a
jrob8577 <roberts.john@gmail.com>
2016-07-20 12:45:10 -0700

    Parsing response from github auth flow

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
-var githubUrl = 'https://github.com/login/oauth/authorize?client_id=a391c044bd23da0e1193'
+var githubUrl = 'https://github.com/login/oauth/authorize?client_id=' + process.env.GITHUB_CLIENT_ID
+var githubTokenRequest = 'https://github.com/login/oauth/access_token'
+
+var githubParameters = function( code ) {
+  return {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code: code
+    })
+  }
+}
+
+var queryStringReducer = function( previous, current, index, array ) {
+  var keyValuePair = current.split( '=' )
+  previous[ keyValuePair[0] ] = keyValuePair[ 1 ]
+
+  return previous
+}
+
+var convertResponseToObject = function( body ) {
+  return body.split( '&' ).reduce( queryStringReducer, {} )
+}
 
 /* GET auth page. */
 router.get('/login', function(req, res, next) {
@@ -13,8 +38,18 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.get('/callback', function(req, res, next) {
-
-  res.send('this is a test of the callback endpoint');
+  fetch( githubTokenRequest, githubParameters( req.query.code) )
+    .then( function( response ) {
+      return response.text()
+    })
+    .then( function( body ) {
+      res.set('Content-Type', 'application/json')
+      res.send( convertResponseToObject( body ) )
+    })
+    .catch( function( error) {
+      // TODO: Do something with this error
+      res.send( error )
+    })
 });
 
 
