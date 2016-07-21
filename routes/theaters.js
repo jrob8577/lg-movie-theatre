3998962d00f2732381eb8c15693bc36e31be83d2
jrob8577 <roberts.john@gmail.com>
2016-07-21 10:19:08 -0700

    Added theater listing and individual theater route

---
+++ routes/theaters.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var Showtimes = require('showtimes')
+
+
+router.get('/:zipCode', function(req, res, next) {
+  var api = new Showtimes( req.params.zipCode, {})
+
+  api.getTheaters( function ( error, theaters ) {
+    if (error) {
+      res.send( error )
+    } else {
+      res.send( theaters )
+    }
+  });
+});
+
+router.get('/id/:id', function( request, response, next ) {
+  var api = new Showtimes()
+
+  var theater = api.getTheater( request.params.id, function( error, theater ) {
+    if( error ) {
+      response.send( error )
+    } else {
+      response.send( theater )
+    }
+  })
+});
+
+module.exports = router;
