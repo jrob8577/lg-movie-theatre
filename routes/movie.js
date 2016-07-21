333e0f8a3c7b29ac42d8aac751f9f544dc636bfd
John Hopkins <johnthopkins@gmail.com>
2016-07-21 16:12:58 -0700

    jrob changed things

--- routes/movie.js
+++ routes/movie.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 var Showtimes = require('showtimes')
 
-
-
 router.get('/:zipCode', function(req, res, next) {
-    var api = new Showtimes(req.params.zipCode, {})
+  var api = new Showtimes(req.params.zipCode, {})
     
  api.getTheaters(function (err, theaters) {
    api.getMovie((theaters[0].movies[0].id), function (err, movie) {
--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
 var Showtimes = require('showtimes')
 
-router.get('/:zipCode-:id-:date', function( request, response, next ) {
-  var api = new Showtimes(request.params.zipCode, {date:request.params.date});
+router.get('/:zipCode/:id/:date', function( request, response, next ) {
+  var api = new Showtimes( request.params.zipCode, { date: request.params.date });
 
   var theater = api.getTheater( request.params.id, function( error, theater ) {
     if( error ) {
@@ -12,12 +13,6 @@ router.get('/:zipCode-:id-:date', function( request, response, next ) {
       response.send( theater )
     }
   })
-// theater.getMovies(function(error, movies){
-// console.log(movies)
-// })
-// 
 });
+
 module.exports = router;
-
-
-//zipCode/:zipCode/
